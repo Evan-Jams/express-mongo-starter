955cd9a008804ceb5237492416e197021abacf35
Evan James <thevan777@gmail.com>
2020-02-29 21:01:52 -0500

    mongo created

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ const PORT = process.env.PORT || 3000;
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODBURI || 'mongodb://localhost:27017/express_app'
-// console.info(MONGODB_URI)
+const MONGODB_URI = process.env['MONGODB_URI'] || 'mongodb://localhost:27017/express_app'
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  {
     useNewUrlParser: true,
